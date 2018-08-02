c0e2e323a09e2c867dc556aa8e1a52edd697dff8
Gus Macfarlane <gusmac@gmail.com>
2018-08-02 16:01:47 +0800

    added the autorisation for home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
  include Pundit
 
  # Pundit: white-list approach.
@@ -13,9 +15,16 @@ class ApplicationController < ActionController::Base
  #   redirect_to(root_path)
  # end
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
+
  private
 
  def skip_pundit?
    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
  end
+
+
 end

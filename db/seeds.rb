cc198af5ff2750b690d4ca6a188553ae9436a651
Julian Leopold <julian@rega-sense.ch>
2018-08-02 16:08:16 +0800

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.new(first_name: "Gus", last_name: "Mac", email: "gusmac@gmail.com", password: "testtest")
+User.new(first_name: "Kevin", last_name: "Smekes", email: "kjsmekens@gmail.com", password: "testtest")
+user1 = User.new(first_name: "Julian", last_name: "Leopold", email: "julian@rega-sense.ch", password: "testtest")
+
+5.times do
+  StorageSpace.create(user: user1, description: "Test", title: "Big storage location", photo: "https://picsum.photos/200/300", capactiy: "15 Elephants", address_city: "Canggu", address_zip_code: "80351", address_country: "Indonesia")
+end

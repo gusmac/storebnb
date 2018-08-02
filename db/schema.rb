288f7ed8addde5f72d25e931dba667832ef69066
Julian Leopold <julian@rega-sense.ch>
2018-08-02 15:32:37 +0800

    added migrate with photo and title

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_01_083849) do
+ActiveRecord::Schema.define(version: 2018_08_02_072715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 2018_08_01_083849) do
     t.string "capacity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
+    t.string "title"
     t.index ["user_id"], name: "index_storage_spaces_on_user_id"
   end
 

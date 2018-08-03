0bf6c17d4d42c0f257910d40cb85f3ea045bcea8
Julian Leopold <julian@rega-sense.ch>
2018-08-03 12:13:51 +0800

    removed migrate post

--- 20141025152200_create_posts.rb
+++
@@ -1,9 +0,0 @@
-class CreatePosts < ActiveRecord::Migration[5.1]
-  def change
-    create_table :posts do |t|
-      t.string    :title
-      t.string    :url
-      t.timestamps # add 2 columns, `created_at` and `updated_at`
-    end
-  end
-end
